5f4e61d8f797d6f98fb88d1904be8b7fc4709c45
unknown <handayaninurhayati9@gmail.com>
2023-04-30 10:10:11 +0700

    tambah file index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+    $nama_kamu = "Hani";
+    $umur = 21;
+    // tanda petik dua
+    /*
+    echo "Hello world, Wellcome ".$nama_kamu;
+    echo " Umur kamu ".$umur;
+    echo " tahun";
+    echo"<br/>";
+    */
+    echo "Hello world, Wellcome "." Umur kamu ".$umur." tahun";
+    echo "<br/>";
+    echo "berarti $nama_kamu lahir di tahun ";
+    echo 2023-$umur;
+
+    // Tanda petik satu
+    //echo 'Hello world';
+?>
